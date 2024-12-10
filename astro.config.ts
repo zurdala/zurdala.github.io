9d62377930b44bb103d213e4306b584b9673271c
Dani <28366329+zurdala@users.noreply.github.com>
2024-12-10 20:17:43 +0100

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -96,7 +96,7 @@ export default defineConfig({
 	// https://docs.astro.build/en/guides/prefetch/
 	prefetch: true,
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://astro-cactus.chriswilliams.dev/",
+	site: "https://zurdala.github.io/",
 	vite: {
 		optimizeDeps: {
 			exclude: ["@resvg/resvg-js"],
